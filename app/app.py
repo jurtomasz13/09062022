a7745565b8e60d108483a2db0d8deb7602876ca8
jurtomasz13 <jur.tomasz13@gmail.com>
2022-06-13 22:02:56 +0200

    Added endpoint to attack player

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
-from calendar import c
+from json import JSONDecodeError
 from starlette.applications import Starlette
-from starlette.responses import JSONResponse, PlainTextResponse
+from starlette.responses import JSONResponse, PlainTextResponse, RedirectResponse
 from starlette.routing import Route, Mount
 from starlette.templating import Jinja2Templates
 from starlette.staticfiles import StaticFiles
+from random import randint
 import crud
 
 
@@ -27,6 +28,35 @@ async def get_players(request):
     return JSONResponse(content=players)
 
 
+async def attack_player(request):
+    LOWER_MULTIPLIER = 0.6
+    HIGHER_MULTIPLIER = 1.4
+    try:
+        data = await request.json()
+        player = crud.get_player_by_name(
+            request.path_params['name'])['players'][0]
+        enemy_player = crud.get_player_by_name(data['name'])['players'][0]
+        while player['hp'] > 0 and enemy_player['hp'] > 0:
+            player['hp'] -= randint(enemy_player['attack_power']
+                                    * LOWER_MULTIPLIER, enemy_player['attack_power']*HIGHER_MULTIPLIER)
+            if player['hp'] > 0 and enemy_player['hp'] > 0:
+                enemy_player['hp'] -= randint(player['attack_power']
+                                              * LOWER_MULTIPLIER, player['attack_power']*HIGHER_MULTIPLIER)
+        if player['hp'] > 0:
+            crud.update_stats(player, enemy_player)
+            message = {'Winner': player['name']}
+        else:
+            crud.update_stats(enemy_player, player)
+            message = {'Winner': enemy_player['name']}
+    except JSONDecodeError:
+        message = {
+            'message': 'You didn\'t specify a player in a path or enemy in a body'}
+    except IndexError:
+        message = {
+            'message': 'Your player or enemy with specified nickname does not exist'}
+    return JSONResponse(content=message)
+
+
 async def html_get_player(request):
     name = request.path_params['name']
     players = crud.get_player_by_name(name)
@@ -43,7 +73,7 @@ async def create_player(request):
     print(player_data)
     crud.create_player(name=player_data['name'], profession=player_data['profession'],
                        hp=player_data['hp'], attack_power=player_data['attack_power'])
-    return PlainTextResponse(content="User created")
+    return RedirectResponse(url='/players')
 
 
 async def html_create_player(request):
@@ -54,6 +84,7 @@ routes = [
     Route('/api/players', get_players),
     Route('/api/player', get_player),
     Route('/api/player', create_player, methods=['POST']),
+    Route('/api/player/{name}/attack', attack_player, methods=['GET', 'POST']),
     Route('/players', html_get_players),
     Route('/player/{name}', html_get_player),
     Route('/player', html_create_player),
