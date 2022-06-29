08ee6abd23aa716b5aef63279c72166450b65009
jurtomasz13 <jur.tomasz13@gmail.com>
2022-06-29 09:05:55 +0200

    Pytest because of current logic forces me to share secret key and algorithm. Will fix this later

--- app/config.py
+++ app/config.py
@@ -6,4 +6,4 @@ JWT_SECRET_KEY = config.get(
     'JWT_SECRET_KEY', default='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiVXNlcl8xIn0.qq-jNXgsfNRmzJN9ppE46LlRP9QYghafAy4spOXysDA')
 JWT_ALGORITHM = config.get('JWT_ALGORITHM', default='HS256')
 JWT_TOKEN_EXPIRE_TIME_IN_MINUTES = config.get(
-    'JWT_TOKEN_EXPIRE_TIME_IN_MINUTES', cast=int, default=30)
+    'JWT_TOKEN_EXPIRE_TIME_IN_MINUTES', cast=int, default=35)
